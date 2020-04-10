7cba041d65d9de54d6554a388d9ea8ecb99ccbf4
sdubois92 <seb.dubois15@free.fr>
2020-04-10 10:25:06 +0200

    My First Game Project

--- snake.js
+++ snake.js
@@ -9,7 +9,14 @@ window.addEventListener("load", function () {
 	for (let i = 0; i < 100; i++) {
 		drawGrid();
 	}
-​
+	// Loading page sound
+	// var startSound = new Audio('./refresh.mp3');
+	// startSound.play();
+	let refreshMusic = new Howl({
+	src: ['./refresh.mp3'],
+	volume: 0.5,
+	});
+	refreshMusic.play();
 	// Création du bouton START
 	let startButton = document.createElement("button");
 	startButton.setAttribute("id", "start");
@@ -23,31 +30,52 @@ window.addEventListener("load", function () {
 	pauseButton.setAttribute("id", "pause");
 	document.getElementById("footer").appendChild(pauseButton);
 	pauseButton.textContent = "PAUSE";
+	// Loading page sound
+	let backgroundMusic = new Howl({
+		src: ['./worldMap.mp3'],
+		volume: 0.6,
+		autoplay:true,
+		loop:true,
+	  });
+	backgroundMusic.play();
 });
-​
+
+
 function startGame() {
-	let pauseButton = document.getElementById("pause");
-	let paused = false;
-​
-	let box = document.getElementsByClassName("box");
-​
+
+	// Resets the score
+	let score = document.querySelector("#score span");
+	score.innerHTML = 0;
+	// Starts the background music
+	let startMusic = new Howl({
+		src: ['./start.mp3'],
+		volume: 2,
+	  });
+	startMusic.play();
+
+	let pauseButton = document.getElementById('pause');
+    let paused = false;
+	let box = document.getElementsByClassName('box');
+	let lose = document.getElementById("lose");
+	lose.style.display = "none";
 	let info = {
 		alive: true,
-		x: 0,
-		y: 0,
-		direction: "right",
-		suprise: true,
+        // x:0,
+        // y:0,
+        direction: "right",
+        suprise: true,
+        score:0,
 	};
-​
+
+	let body = [
+		{x:0,y:0}
+	]
+
 	let appleIndex = {
 		x: 0,
 		y: 0,
 	};
-​
-	let snakeLength = 1;
-​
 	const keyState = {};
-​
 	window.onkeydown = function (event) {
 		// ou window.onkeydown = (e) => {keyState[e.code] = true;}
 		keyState[event.code] = true;
@@ -55,7 +83,6 @@ function startGame() {
 	window.onkeyup = function (event) {
 		keyState[event.code] = false;
 	};
-​
 	// Affiliée à window.onkeyup/onkeydown, donc il est pris en compte à chaque fois que l'on appuie sur une touche
 	function changeDirection() {
 		if (keyState["ArrowDown"]) {
@@ -68,122 +95,204 @@ function startGame() {
 			info.direction = "right";
 		}
 	}
-​
 	setInterval(() => {
 		changeDirection();
 	}, 1);
-​
 	function gameLoop() {
-		// Snake is alive
 		let snakeAlive = info.alive;
+		// Pauses the game with the pauseButton
 		let intervalId = 0;
 		pauseButton.addEventListener("click", pause);
-​
 		function pause() {
 			if (paused) {
 				intervalId = setInterval(maLoop, 300);
+
+				// Son
+				let stopSound = new Howl({
+					src: ['./clicksound.mp3'],
+					volume: 2,
+				});
+				stopSound.play()
+
 				paused = false;
 			} else {
 				clearInterval(intervalId);
+
+				let stopSound = new Howl({
+					src: ['./clicksound.mp3'],
+					volume: 2,
+				  });
+				  stopSound.play();
+
 				paused = true;
 			}
 		}
-​
-		// prompt("Appuyer sur une touche pour reprendre la partie")
-​
 		// First Apple appears and Snake too
 		apple = surpriseAppears();
-		box[info.x + info.y].style.background =
+		box[(body[0].x) + (body[0].y)].style.background =
 			"pink url('./Lapinpin.png') no-repeat center center / contain";
-​
 		function surpriseAppears() {
 			appleIndex.x = Math.floor(Math.random() * 10); // Chiffre 0-9
 			appleIndex.y = Math.floor(Math.random() * 10) * 10; // Nombre 10-90 par dizaines
+			
+			if ( Math.floor(Math.random()*3) === 0 ) {
 			box[appleIndex.x + appleIndex.y].style.background =
-				"#A880C5 url('./loup.png') no-repeat center center / contain";
+				"lightgrey url('./elephant.png') no-repeat center center / contain";
+			} else if (Math.floor(Math.random()*3) === 1) {
+				box[appleIndex.x + appleIndex.y].style.background =
+					"#A880C5 url('./loup.png') no-repeat center center / contain";
+			} else {
+				box[appleIndex.x + appleIndex.y].style.background =
+					"black url('./Lapinpin.png') no-repeat center center / contain";
+			}
 			info.surprise = true;
 		}
-​
 		function checkSurprise() {
-			if (box[info.x + info.y] === box[appleIndex.x + appleIndex.y]) {
-				snakeLength++;
-				info.surprise = false;
-				surpriseAppears();
+			if (box[(body[0].x) + (body[0].y)] === box[appleIndex.x + appleIndex.y]) {
+				let eatSound = new Howl({
+					src: ['./eat.mp3'],
+					volume: 1,
+				});
+				eatSound.play()
+
+				// On pushe un nouvel objet pour une queue lorsqu'on mange une pomme
+				body.push( { x:body[0].x, y: body[0].y });
+
+                info.surprise = false;
+                surpriseAppears();
+                info.score++;
+                score.innerHTML = info.score;
 			}
 		}
-​
-		// Boucle du jeu
+		// Boucle du jeu : met setInterval dans une variable initialisée à 0
 		intervalId = setInterval(maLoop, 300);
 		function maLoop() {
-			console.log("boucle gameplay");
-​
-			// Pauses the game with the pauseButton
-			// pauseButton.addEventListener("click", pause);
-​
-			// function pause() {
-			// 	if (!paused) {
-			// 		paused = true;
-			// 	} else if (paused) {
-			// 		paused = false;
-			// 	}
-			// }
-			// if (paused) {
-			// 	return;
-			// }
-​
 			if (snakeAlive) {
 				//Checks if the Apple is eaten
 				checkSurprise();
-​
+
+				// AJOUTER FONCTION ICI pour la tail avec boucle, on met ça ici pour récupérer la position avant qu'il avance
+				// Donc sa position n-1, qu'on va récupérer après pour la première queue
+				let formerHeadPositionX = body[0].x;
+				let formerHeadPositionY = body[0].y;
+				let previousTailX;
+				let previousTailY;
+
 				switch (info.direction) {
 					case "right":
 						// On définit x et y, et on les sépare dans la box[x+y], car si on n'avait que des x, on ne pourrait pas tourner à droite
 						// par exemple en étant sur la 2e ligne, car x serait plus grand que 10
 						// Ici, si on est case 55, alors "y" représente les dizaines et "x" les unités
-						box[info.x + info.y].style.background = "rgb(165,202,73)";
-						info.x += 1;
-						if (info.x >= 10) {
-							info.x = 0;
+						box[(body[0].x) + (body[0].y)].style.background = "rgb(165,202,73)";
+						body[0].x += 1;
+						if (body[0].x >= 10) {
+							body[0].x = 0;
 						}
-						box[info.x + info.y].style.background =
+						box[(body[0].x) + (body[0].y)].style.background =
 							"pink url('./Lapinpin.png') no-repeat center center / contain";
 						break;
-​
 					case "left":
-						box[info.x + info.y].style.background = "rgb(165,202,73)";
-						if (info.x <= 0) {
-							info.x = 10;
+						box[(body[0].x) + (body[0].y)].style.background = "rgb(165,202,73)";
+						if (body[0].x <= 0) {
+							body[0].x = 10;
 						}
-						info.x -= 1;
-						box[info.x + info.y].style.background =
+						body[0].x -= 1;
+						box[(body[0].x) + (body[0].y)].style.background =
 							"pink url('./Lapinpin.png') no-repeat center center / contain";
 						break;
-​
 					case "up":
-						box[info.x + info.y].style.background = "rgb(165,202,73)";
-						info.y -= 10;
-						if (info.y <= -10) {
-							info.y = 90;
+						box[(body[0].x) + (body[0].y)].style.background = "rgb(165,202,73)";
+						body[0].y -= 10;
+						if (body[0].y <= -10) {
+							body[0].y = 90;
 						}
-						box[info.x + info.y].style.background =
+						box[(body[0].x) + (body[0].y)].style.background =
 							"pink url('./Lapinpin.png') no-repeat center center / contain";
 						break;
-​
 					case "down":
-						box[info.x + info.y].style.background = "rgb(165,202,73)";
-						info.y += 10;
-						if (info.y >= 100) {
-							info.y = 0;
+						box[(body[0].x) + (body[0].y)].style.background = "rgb(165,202,73)";
+						body[0].y += 10;
+						if (body[0].y >= 100) {
+							body[0].y = 0;
 						}
-						box[info.x + info.y].style.background =
+						box[(body[0].x) + (body[0].y)].style.background =
 							"pink url('./Lapinpin.png') no-repeat center center / contain";
 						break;
 				}
-			} else if (!snakeAlive) {
-				console.log("You lost");
+				// ICI on reprend les 
+				
+				
+				body.forEach(function (tail, index) {
+					if (index === 0) {return;}
+					if (index === 1) {
+					box[(body[index].x) + (body[index].y)].style.background = "rgb(165,202,73)";
+					// On stocke les valeurs actuelles de la première queue avant changement dans previousTailX/Y
+					// On déclare les 2 variables previousTailX/Y en dehors, car sinon on ne peut pas y accéder dans le else
+					// previousTailX/Y SERT SIMPLEMENT POUR FAIRE APPARAITRE L'INDEX 2
+					previousTailX = tail.x; // 0 & 1
+					previousTailY = tail.y;
+					// La première queue reprend l'ancienne position de la tête
+					tail.x = formerHeadPositionX; // 0 & 1
+					tail.y = formerHeadPositionY;
+					
+					box[(body[index].x) + (body[index].y)].style.background = "pink url('./Lapinpin.png') no-repeat center center / contain";
+
+					} else {
+						box[(body[index].x) + (body[index].y)].style.background = "rgb(165,202,73)";
+						// Ici, on déclare les variables des valeurs actuelles de la queue de l'index, qui seront ensuite prises par les suivantes
+						let tailAfterX = tail.x; // undefined & 1
+						let tailAfterY = tail.y;
+						// La queue suivante prend la position de la queue d'avant au tour d'avant
+						tail.x = previousTailX; // undefined & 1
+						tail.y = previousTailY;
+						// 
+						previousTailX = tailAfterX; // undefined & 1
+						previousTailY = tailAfterY;
+
+						box[(body[index].x) + (body[index].y)].style.background = "pink url('./Lapinpin.png') no-repeat center center / contain";
+
+						// Si on se touche la queue
+						if (body[0].x === tailAfterX && body[0].y === tailAfterY) {
+							snakeAlive = false;
+							clearInterval(intervalId);
+							lose.style.display = "block";
+							lose.innerHTML = `YOUR SCORE = ${info.score}`;
+							let reset = document.getElementById("start");
+							reset.textContent = "RESET";
+							for (let i = 0; i<100; i++) {
+							box[i].style.background = "rgb(165,202,73)";
+							}
+							reset.addEventListener("click", startGame, {
+								once: true, // Just once
+							});
+						}
+
+						if (body[0].x === previousTailX && body[0].y === previousTailY) {
+							snakeAlive = false;
+							clearInterval(intervalId);
+							lose.style.display = "block";
+							lose.innerHTML = `YOUR SCORE = ${info.score}`;
+							let reset = document.getElementById("start");
+							reset.textContent = "RESET";
+							for (let i = 0; i<100; i++) {
+								box[i].style.background = "rgb(165,202,73)";
+								}
+							reset.addEventListener("click", startGame, {
+								once: true, // Just once
+							});
+						}
+					}
+
+				});
+
 			}
+			// 	else if (!snakeAlive) {
+				// 	clearInterval(intervalId);
+				// 	lose.style.display = "block";
+				// 	lose.innerHTML = `YOUR SCORE = ${info.score}`;
+			// }
 		}
-		// event.preventDefault(); // Prevents the arrow keys from scrolling the window
+		// event.preventDefault(); // Prevents the arrow keys from scrolling the window (only if bigger than the window)
 	}
 	gameLoop();
 }
--- snake2.js
+++ snake2.js
@@ -398,87 +398,77 @@ function checkBoops(rect1, rect2) {
 
 
 
-  window.addEventListener("load", function() {
-
-    // Création du board
-    let container = document.getElementById('grid-container');
-    function drawGrid() {
-        let box = document.createElement('div');
-        box.classList.add('box');
-        container.appendChild(box);
-    }
-    for (let i = 0; i<100; i++) {
-        drawGrid();
-    }
-
-    // Création du bouton START
-    let startButton = document.createElement('button');
-    startButton.setAttribute("id", "start");
-    document.getElementById('header').appendChild(startButton);
-    startButton.textContent = "START";
-    startButton.addEventListener("click", startGame, {
-        once: true, // Just once
-    });
-    // Création du bouton PAUSE
-    let pauseButton = document.createElement('button');
-    pauseButton.setAttribute('id','pause');
-    document.getElementById('footer').appendChild(pauseButton);
-    pauseButton.textContent = "PAUSE";
+  window.addEventListener("load", function () {
+	// Création du board
+	let container = document.getElementById("grid-container");
+	function drawGrid() {
+		let box = document.createElement("div");
+		box.classList.add("box");
+		container.appendChild(box);
+	}
+	for (let i = 0; i < 100; i++) {
+		drawGrid();
+	}
+	// Création du bouton START
+	let startButton = document.createElement("button");
+	startButton.setAttribute("id", "start");
+	document.getElementById("header").appendChild(startButton);
+	startButton.textContent = "START";
+	startButton.addEventListener("click", startGame, {
+		once: true, // Just once
+	});
+	// Création du bouton PAUSE
+	let pauseButton = document.createElement("button");
+	pauseButton.setAttribute("id", "pause");
+	document.getElementById("footer").appendChild(pauseButton);
+	pauseButton.textContent = "PAUSE";
 });
-
-
 function startGame() {
-
-    let pauseButton = document.getElementById('pause');
+	let pauseButton = document.getElementById('pause');
     let paused = false;
     let score = document.querySelector("#score span");
     let box = document.getElementsByClassName('box');
-    let info = {
-        alive: true,
+	let info = {
+		alive: true,
         x:0,
         y:0,
         direction: "right",
         suprise: true,
         score:0,
         snakeLength:1
-    }
-    let appleIndex = {
-        x:0,
-        y:0
-    };
-    const keyState = {};
-    window.onkeydown = function(event) {   // ou window.onkeydown = (e) => {keyState[e.code] = true;}
-    keyState[event.code] = true;
-    }
-    window.onkeyup = function(event) {
-        keyState[event.code] = false;
-    }
-
-    // Affiliée à window.onkeyup/onkeydown, donc il est pris en compte à chaque fois que l'on appuie sur une touche
-    function changeDirection(){
-
-        if (keyState["ArrowDown"]) {
-            info.direction = "down";
-        } else if (keyState["ArrowUp"]) {
-            info.direction = "up";
-        } else if (keyState["ArrowLeft"]) {
-            info.direction = "left";
-        } else if (keyState["ArrowRight"]) {
-            info.direction = "right";
-        }
-    }
-    
-    setInterval(() => {
-        changeDirection();
-    },1)
-    
-
-    function gameLoop () {
-        // Snake is alive
-        let snakeAlive = info.alive;
-        let intervalId = 0;
+	};
+	let appleIndex = {
+		x: 0,
+		y: 0,
+	};
+	const keyState = {};
+	window.onkeydown = function (event) {
+		// ou window.onkeydown = (e) => {keyState[e.code] = true;}
+		keyState[event.code] = true;
+	};
+	window.onkeyup = function (event) {
+		keyState[event.code] = false;
+	};
+	// Affiliée à window.onkeyup/onkeydown, donc il est pris en compte à chaque fois que l'on appuie sur une touche
+	function changeDirection() {
+		if (keyState["ArrowDown"]) {
+			info.direction = "down";
+		} else if (keyState["ArrowUp"]) {
+			info.direction = "up";
+		} else if (keyState["ArrowLeft"]) {
+			info.direction = "left";
+		} else if (keyState["ArrowRight"]) {
+			info.direction = "right";
+		}
+	}
+	setInterval(() => {
+		changeDirection();
+	}, 1);
+	function gameLoop() {
+		let snakeAlive = info.alive;
+		// Pauses the game with the pauseButton
+		let intervalId = 0;
 		pauseButton.addEventListener("click", pause);
-​
 		function pause() {
 			if (paused) {
 				intervalId = setInterval(maLoop, 300);
@@ -488,102 +478,148 @@ function startGame() {
 				paused = true;
 			}
 		}
-
-        // First Apple appears and Snake too
-        apple = surpriseAppears();
-        box[info.x + info.y].style.background = "pink url('./Lapinpin.png') no-repeat center center / contain";
-
-        function surpriseAppears () {
-            appleIndex.x = Math.floor(Math.random()*10);    // Chiffre 0-9
-            appleIndex.y = Math.floor(Math.random()*10)*10; // Nombre 10-90 par dizaines
-            box[appleIndex.x + appleIndex.y].style.background = "#A880C5 url('./loup.png') no-repeat center center / contain";
-            info.surprise = true;
-        }
-
-        function checkSurprise () {
-            if (box[info.x + info.y] === box[appleIndex.x + appleIndex.y]) {
-                info.snakeLength+=1;
+		// First Apple appears and Snake too
+		apple = surpriseAppears();
+		box[info.x + info.y].style.background =
+			"pink url('./Lapinpin.png') no-repeat center center / contain";
+		function surpriseAppears() {
+			appleIndex.x = Math.floor(Math.random() * 10); // Chiffre 0-9
+			appleIndex.y = Math.floor(Math.random() * 10) * 10; // Nombre 10-90 par dizaines
+			
+			if ( Math.floor(Math.random()*3) === 0 ) {
+			box[appleIndex.x + appleIndex.y].style.background =
+				"lightgrey url('./elephant.png') no-repeat center center / contain";
+			} else if (Math.floor(Math.random()*3) === 1) {
+				box[appleIndex.x + appleIndex.y].style.background =
+					"#A880C5 url('./loup.png') no-repeat center center / contain";
+			} else {
+				box[appleIndex.x + appleIndex.y].style.background =
+					"black url('./Lapinpin.png') no-repeat center center / contain";
+			}
+			info.surprise = true;
+		}
+		function checkSurprise() {
+			if (box[info.x + info.y] === box[appleIndex.x + appleIndex.y]) {
+                // switch (info.direction) {
+				// 	case "right":
+				// 		body.push( { x:(body[0].x)-1, y: body[0].y });
+				// 		break;
+				// 	case "left":
+				// 		body.push( { x:(body[0].x)+1, y: body[0].y });
+				// 		break;
+				// 	case "up":
+				// 		body.push( { x:body[0].x, y: (body[0].y)+10 });
+				// 		break;
+				// 	case "down":
+				// 		body.push( { x:body[0].x, y: (body[0].y)-10 });
+				// 		// box[(body[body.length-1].x) + (body[body.length-1].y)].style.background = "pink url('./Lapinpin.png') no-repeat center center / contain";
+				// 		break;
+				// }
+				info.snakeLength+=1;
                 info.surprise = false;
                 surpriseAppears();
                 info.score++;
                 score.innerHTML = info.score;
-            }
-        }
-        
-        
-        // Boucle du jeu
-        intervalId = setInterval(maLoop, 300);
-            function maLoop() {
+			}
+		}
+		// Boucle du jeu : met setInterval dans une variable initialisée à 0
+		intervalId = setInterval(maLoop, 300);
+		function maLoop() {
 
-             // Pauses the game with the pauseButton
-            // pauseButton.addEventListener('click', pause);
-
-            // function pause () {
-            //     if (!paused) {
-            //         paused = true;
-            //     } else if (paused) {
-            //         paused = false;
-            //     }
-            // }
-            // if (paused) {return;}
-        
-
-            if (snakeAlive) {
-        
-        //Checks if the Apple is eaten
-                checkSurprise();
-
-                switch (info.direction) {
-                    case "right":
-                        // On définit x et y, et on les sépare dans la box[x+y], car si on n'avait que des x, on ne pourrait pas tourner à droite
-                        // par exemple en étant sur la 2e ligne, car x serait plus grand que 10
-                        // Ici, si on est case 55, alors "y" représente les dizaines et "x" les unités
-                        box[info.x + info.y].style.background = "rgb(165,202,73)";
-                        info.x += 1;
-                        if(info.x >= 10){
-                            info.x = 0;
-                        }
-                        box[info.x + info.y].style.background = "pink url('./Lapinpin.png') no-repeat center center / contain";
-                        break;
-
-                    case "left":
-                        box[info.x + info.y].style.background = "rgb(165,202,73)";
-                        if(info.x <= 0){
-                            info.x = 10;
-                        }
-                        info.x -= 1;
-                        box[info.x + info.y].style.background = "pink url('./Lapinpin.png') no-repeat center center / contain";
-                        break;
-
-                    case "up":
-                        box[info.x + info.y].style.background = "rgb(165,202,73)";
-                        info.y -= 10;
-                        if(info.y<=-10) {
-                           info.y=90;
-                        }
-                        box[info.x + info.y].style.background = "pink url('./Lapinpin.png') no-repeat center center / contain";
-                        break;
-
-                    case "down":
-                        box[info.x + info.y].style.background = "rgb(165,202,73)";
-                        info.y +=10;
-                        if (info.y >= 100) {
-                            info.y = 0;
-                        }
-                        box[info.x + info.y].style.background = "pink url('./Lapinpin.png') no-repeat center center / contain";
-                        break;
-                }
-            
-         
-    
-            } else if (!snakeAlive) {
-                console.log("You lost");
-            }
-        
-    }
-
-    // event.preventDefault(); // Prevents the arrow keys from scrolling the window
-    }
-    gameLoop();
+			if (snakeAlive) {
+				//Checks if the Apple is eaten
+				checkSurprise();
+				switch (info.direction) {
+					case "right":
+						// On définit x et y, et on les sépare dans la box[x+y], car si on n'avait que des x, on ne pourrait pas tourner à droite
+						// par exemple en étant sur la 2e ligne, car x serait plus grand que 10
+						// Ici, si on est case 55, alors "y" représente les dizaines et "x" les unités
+						box[info.x + info.y].style.background = "rgb(165,202,73)";
+						info.x += 1;
+						if (info.x >= 10) {
+							info.x = 0;
+						}
+						box[info.x + info.y].style.background =
+							"pink url('./Lapinpin.png') no-repeat center center / contain";
+						break;
+					case "left":
+						box[info.x + info.y].style.background = "rgb(165,202,73)";
+						if (info.x <= 0) {
+							info.x = 10;
+						}
+						info.x -= 1;
+						box[info.x + info.y].style.background =
+							"pink url('./Lapinpin.png') no-repeat center center / contain";
+						break;
+					case "up":
+						box[info.x + info.y].style.background = "rgb(165,202,73)";
+						info.y -= 10;
+						if (info.y <= -10) {
+							info.y = 90;
+						}
+						box[info.x + info.y].style.background =
+							"pink url('./Lapinpin.png') no-repeat center center / contain";
+						break;
+					case "down":
+						box[info.x + info.y].style.background = "rgb(165,202,73)";
+						info.y += 10;
+						if (info.y >= 100) {
+							info.y = 0;
+						}
+						box[info.x + info.y].style.background =
+							"pink url('./Lapinpin.png') no-repeat center center / contain";
+						break;
+				}
+			} else if (!snakeAlive) {
+				console.log("You lost");
+			}
+		}
+		// event.preventDefault(); // Prevents the arrow keys from scrolling the window
+	}
+	gameLoop();
 }
 
+
+
+Yesterday I took care of all the basic functionalities such as moving and the scoring and pausing,
+now I still have either the styling to improve or to focus on the most complicated part: the head + tail and the size
+
+Add Array = [ {x:8 y:8}, {} ]
+
+head is always [0.x + 0.y]
+tail is always []
+
+
+
+Faire bouger queue avec tête :
+
+let oldHeadPositionX = body[0].x;
+let oldHeadPositionY = body[0].y;
+
+Puis :
+let previousTailX;
+let previousTailY;
+
+for (let i=0;i<this.tail.length;i++) {
+    previousTailX = this.tail[i].x;
+    previousTailY = this.tail[i].y;
+    if (i === 0) {
+        previousTailX = this.tail[i].x;
+        previousTailY = this.tail[i].y;
+        this.tail[i].x = oldHeadPositionX;
+        this.tail[i].y = oldHeadPositionY;
+    } else {
+        let oldY = this.tail[i].y;
+        let oldX = this.tail[i].x;
+        this.tail[i].x = previousTailX;
+        this.tail[i].y = previousTailY;
+        previousTailX = oldX;
+        previousTailY = oldY;
+    }
+}
+}
+
+
+// Fichier audio 100% JS
+var audio = new Audio('audio.file.mp3');
+audio.play()
